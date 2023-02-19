957bf5adef38402fbfaf8a37dae8716f9102758d
一只小猿猿 <ryh15234883064@foxmail.com>
2023-02-19 20:11:16 +0800

    新增底部栏

--- blog-web/src/views/HomePage.vue
+++ blog-web/src/views/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="home">
     <base-header/>
+    <base-footer />
   </div>
 </template>
 
 <script>
 import baseHeader from "@/components/header/BaseHeader.vue";
+import baseFooter from "@/components/fooder/BaseFooter.vue";
 export default {
   name: "HomePage",
   components:{
-    'base-header': baseHeader
+    'base-header': baseHeader,
+    'base-footer': baseFooter
   }
 }
 </script>
